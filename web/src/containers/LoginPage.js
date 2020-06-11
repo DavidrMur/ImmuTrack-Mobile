04c10983c66d5f6911e488588f8e5205eafc18d5
davidrmur <davidrmurga@gmail.com>
2020-06-11 16:22:55 -0400

    connected to redux store and saga (functional)

--- web/src/containers/LoginPage.js
+++ web/src/containers/LoginPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'redux-saga-store/actions/index';
 import './LoginPage.css';
 
 class LoginPage extends Component {
@@ -9,18 +11,23 @@ class LoginPage extends Component {
     }
     
     setValue = (e, key) => {
-        debugger;
+        e.preventDefault();
         this.setState({[key]: e.target.value})
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.authPending(this.state.username, this.state.password);
     }
 
     render(){
         return(
             <div>
-                <form className='container'>
+                <form className='container' onSubmit={(e) => this.onSubmit(e)}>
                     <h1>Member Login</h1>
                     <input type="text" placeholder="username" className='container--field' onChange={(event) => this.setValue(event, 'username')}/>
                     <input type="password" placeholder="password" className='container--field' onChange={(event) => this.setValue(event, 'password')}/>
-                    <input type="submit" value="Login" className='container--button' />
+                    <input type="submit" value="Login" className='container--button' onClick={(e) => this.onSubmit(e)}/>
                 </form>
                 <label>Remember Me</label>
                 <input type="checkbox" />
@@ -42,8 +49,9 @@ const mapStateToProps = state => {
 
 const mapDispathToProps = dispatch => {
     return {
+        authPending: (username, password) => dispatch(actions.authPending(username, password)) 
     };
 };
 
 //export default connect(mapStateToProps,mapDispathToProps)(SummonerProfile);
-export default LoginPage
+export default connect(null,mapDispathToProps)(LoginPage)
