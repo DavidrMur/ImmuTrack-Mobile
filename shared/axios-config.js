8b80fe15cb05b4391ba425e799d7eafc24c93adc
davidrmur <davidrmurga@gmail.com>
2020-07-16 16:37:49 -0400

    main - remove useless line

--- shared/axios-config.js
+++ shared/axios-config.js
@@ -3,10 +3,6 @@ import axios from 'axios';
 const instance = axios.create();
 instance.interceptors.request.use(function (config) {
     let newConfig = {...config}
-    if (typeof(newConfig.data) !== "object") {
-        let data = newConfig.data;
-        newConfig.data = {data}
-    }
     if (localStorage.getItem("jwtToken") !== null) newConfig.data.token = localStorage.getItem("jwtToken");
     return newConfig
 });
