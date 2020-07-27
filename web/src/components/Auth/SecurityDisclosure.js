a3e504094a6c6fdbc176938688c1dea837a4a7f4
davidrmur <davidrmurga@gmail.com>
2020-07-27 12:23:10 -0400

    auth - adding security disclosure

--- web/src/components/Auth/SecurityDisclosure.js
+++ web/src/components/Auth/SecurityDisclosure.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import { Grid, Button, TextField } from '@material-ui/core'
+import { Grid, Button, Typography } from '@material-ui/core'
 
 class SecurityDisclosure extends Component {
 
+    onClick = () => {
+        window.location.reload(false)
+        localStorage.setItem('acceptDisclosure', true)
+    }
+    
     render() {
         return (
-            <Grid container spacing={4} style={{'text-align': 'center', 'width': '80vw', 'margin': 'auto'}}>
-                <Grid item xs={6} >
-                    <TextField>wef</TextField>
-                    <Button onClick={() => localStorage.setItem('acceptDisclosure', true)}>I Accept</Button>
+            <Grid container spacing={8} style={{'text-align': 'left', 'margin': 'auto', 'width': '80vw'}}>
+                <Grid item xs={12} >
+                    <Typography variant="h2">Acceptable Use Policy</Typography>
+                    <Typography variant="paragraph">oewifjoweijfowiejfqofijwe</Typography>
+                    <Typography variant="h4">CONSENT TO DISCLOSE YOUR PERSONAL HEALTH INFORMATION</Typography>
+                    <Typography variant="paragraph">oewifjoweijfowiejfqofijwe</Typography>
+                    <Typography variant="h4">ACCESS AND USE</Typography>
+                    <Typography variant="paragraph">oewifjoweijfowiejfqofijwe</Typography>
+                    <Typography variant="h4">SECURITY</Typography>
+                    <Typography variant="paragraph">oewifjoweijfowiejfqofijwe</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button onClick={() => this.onClick()}>I Accept</Button>
                     <Button>I Do Not Accept</Button>
                 </Grid>
                 
