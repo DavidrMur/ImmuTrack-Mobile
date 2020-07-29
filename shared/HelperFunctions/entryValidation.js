83979102c7b30dd8ff35aa3e655332f19afbf9d1
davidrmur <davidrmurga@gmail.com>
2020-07-29 12:52:33 -0400

    auth - small corrections

--- shared/HelperFunctions/entryValidation.js
+++ shared/HelperFunctions/entryValidation.js
@@ -5,7 +5,9 @@ const entryValidation = (e, type) => {
         case 'username':
             return usernameValidation(e.target.value)
         case 'password':
-            return passwordValidation(e.target.value)    
+            return passwordValidation(e.target.value)
+        case 'scn':
+            return scnValidation(e.target.value)         
         case 'fName':
         case 'lName':   
         default:
@@ -18,14 +20,20 @@ const usernameValidation = (value) => {
 
     if(value.length < 6 || !re.test(value)){
         return false;
-    }
+    } else return true
 }
 
 const passwordValidation = (value) => {
 
     if(value.length < 8){
         return false;
-    }
+    } else return true
+}
+
+const scnValidation = (value) => {
+    if (value.length < 9) {
+        return false;
+    } else return true
 }
 
 export default entryValidation;
