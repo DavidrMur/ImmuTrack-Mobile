48e94fb7d14db997b50a6532583aae2fc6eb070a
davidrmur <davidrmurga@gmail.com>
2020-08-06 17:19:35 -0400

    main - vaccine & bacteria pt3

--- shared/store/sagas/immunization.js
+++ shared/store/sagas/immunization.js
@@ -53,6 +53,8 @@ export function* patientUpdateInfoPending(action){
     try {
         console.log ('Saga response');
         console.log(action);
+        let temp = {vaccine: 'Buttery Bongs', bacteria: ['ur mum', 'get rekt']};
+        let temp2 = yield axios.post("http://127.0.0.1:5000/addVaccine", temp);
         let response = yield axios.post("http://127.0.0.1:5000/editPatientRecord", action.payload);
         // gonna call the api again for updated page?
         //yield put (actions.patientAddEntrySuccess(action.payload))
