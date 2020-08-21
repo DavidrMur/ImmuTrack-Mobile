bfbaa572c10278618a767fdd99becca157284772
macdoc26 <60278653+macdoc26@users.noreply.github.com>
2020-08-21 13:48:01 -0400

    README.md completed, included contact information

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ImmuTrack-Frontend
-Both the web and mobile are using React and Node. Make sure you have node.js installed.
+Both the web and mobile are using React and Node. Make sure you have node.js installed. For questions concerning the frontend please contact; 
+
+David Murga (React-native) `davidrmurga@gmail.com`
+
+Cameron Macdonald (Styling and Material-ui) `macdoc26@mcmaster.ca`
 
 ## To run
 1. Clone the repository either using GitHub or downloading the repository
@@ -7,3 +11,23 @@ Both the web and mobile are using React and Node. Make sure you have node.js ins
 3. ```npm install```
 4. ```npm start```
 
+## File Structure
+### Mobile
+`App.js` App file for mobile
+#### Containers
+`LoginPage.js` mobile login file
+
+### Web
+`App.js` File that includes the main features of the app
+#### src
+##### components
+`Auth` Component files for authentication including the login page and the security disclosure
+
+`Immunization` Component files or all of the patient and provider pages related to immunisation including the home page and the record page
+
+`Shared` Includes the header components
+
+#### containers
+`Auth` Authentication container including; Forgot password, login and signup
+
+`Immunization` Container pages for the healthcare and patient pages
--- web/src/components/Auth/SignupPage/SignupPageHealthcare.js
+++ web/src/components/Auth/SignupPage/SignupPageHealthcare.js
@@ -18,37 +18,39 @@ class SignupPageHealthcare extends Component {
 
     render() {
         return (
-            <Grid container spacing={4} style={{'text-align': 'center', 'width': '80vw', 'margin': 'auto'}}>
-                <Grid item xs={6} >
-                    <SignupProfession fieldFunction = {this.props.healthcareSignupFunctions.signupSetProfession} />
-                    <SignupTitle fieldFunction = {this.props.healthcareSignupFunctions.signupSetTitle} />
-                </Grid>
-                <Grid item xs={6} >
-                    <SignupLicense fieldFunction = {this.props.healthcareSignupFunctions.signupSetLicense}/>
-                </Grid>
-                <Grid item xs={6}>
-                    <SignupName nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetName} />
-                </Grid>
-                <Grid item xs={6} />
-                <Grid item xs={6}>
-                    <SignupPrimaryWork nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetWork} index={0}/>
-                </Grid> 
-                {<>{Array(this.state.workLocationCount).fill(1).map((input,i)=>(
+            <form>
+                <Grid container spacing={4} style={{'text-align': 'center', 'width': '80vw', 'margin': 'auto'}}>
+                    <Grid item xs={6} >
+                        <SignupProfession fieldFunction = {this.props.healthcareSignupFunctions.signupSetProfession} />
+                        <SignupTitle fieldFunction = {this.props.healthcareSignupFunctions.signupSetTitle} />
+                    </Grid>
+                    <Grid item xs={6} >
+                        <SignupLicense fieldFunction = {this.props.healthcareSignupFunctions.signupSetLicense}/>
+                    </Grid>
                     <Grid item xs={6}>
-                        <SignupOtherWork nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetWork} index={i+1}/>
-                    </Grid>))}
-                </>}
-                <Grid item xs={6}>
-                    <Button onClick={() => this.setState({workLocationCount: this.state.workLocationCount + 1})}>Add Work Location</Button>
+                        <SignupName nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetName} />
+                    </Grid>
+                    <Grid item xs={6} />
+                    <Grid item xs={6}>
+                        <SignupPrimaryWork nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetWork} index={0}/>
+                    </Grid>
+                    {<>{Array(this.state.workLocationCount).fill(1).map((input,i)=>(
+                        <Grid item xs={6}>
+                            <SignupOtherWork nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetWork} index={i+1}/>
+                        </Grid>))}
+                    </>}
+                    <Grid item xs={6}>
+                        <Button onClick={() => this.setState({workLocationCount: this.state.workLocationCount + 1})}>Add Work Location</Button>
+                    </Grid>
+                    {this.state.workLocationCount >= 1 ? <Grid item xs={6}>
+                        <Button onClick={() => this.removeWorkLocation()}>Remove Work Location</Button>
+                    </Grid> : null}
+                    <Grid item xs={6}>
+                        <SignupCredentials nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetCredentials}/>
+                    </Grid>
+                   <Button variant={'outlined'} onClick={this.props.signup}>Signup</Button>
                 </Grid>
-                {this.state.workLocationCount >= 1 ? <Grid item xs={6}>
-                    <Button onClick={() => this.removeWorkLocation()}>Remove Work Location</Button>
-                </Grid> : null}
-                <Grid item xs={6}>
-                    <SignupCredentials nestedFieldFunction = {this.props.healthcareSignupFunctions.signupSetCredentials}/>
-                </Grid>
-               <Button onClick={this.props.signup}>Signup</Button> 
-            </Grid>
+            </form>
         )
     }
 
